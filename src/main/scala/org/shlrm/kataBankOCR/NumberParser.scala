6b287c245c664169e328e0e72f3778b546374f68
David Kowis <dkowis@shlrm.org>
2013-02-12 23:38:04 -0600

    Got part of the implementation
    Only loading in lines, don't actually have it parsing the individual
    numbers out. But yay

--- src/main/scala/org/shlrm/kataBankOCR/NumberParser.scala
+++ src/main/scala/org/shlrm/kataBankOCR/NumberParser.scala
@@ -1,13 +1,41 @@
 package org.shlrm.kataBankOCR
 
-class NumberParser(fileName: String) {
+import io.Source
 
-  def loadFile = {
-    val source = scala.io.Source.fromFile(fileName)
-    val lines = source.getLines()
-    source.close()
+class NumberParser() {
+
+  /**
+   * Lets read in the entire file, and get a List of List of strings, 3 lines for each entry
+   * @return
+   */
+  def loadFile(source: Source): List[List[String]] = {
+    val grouped = source.getLines().grouped(4)
+    grouped.foldLeft(List[List[String]]())((coll, strings) => {
+      strings.toList :: coll
+    }).reverse
   }
 
-  def value:Int = ???
+  /**
+   * Take an account number and return the nine digits that are it
+   * Get a list of list of strings, then we can go through each one and determine if it properly matches things
+   * @param lines
+   */
+  def toNumber(lines:List[List[String]]) = {
+    /*
+    Convert the list of strings to a matrix of things that look like LED numbers
+    Specifically a list of two dimensional arrays (or whatever the non mutable thing is for that)
+    x_x 010
+    |_| 111
+    |_| 111
+     */
+
+
+  }
+
+  def value: Int = {
+
+
+    0
+  }
 
 }
