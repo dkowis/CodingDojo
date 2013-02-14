282ab7af55842a2692bc23b4c6e265665a5c985d
David Kowis <dkowis@shlrm.org>
2013-02-14 01:43:34 -0600

    Updating the numbers file as per the hints
    Just makes it easier to look at the harcoded values

--- KataBankOCR/src/main/scala/org/shlrm/kataBankOCR/Numbers.scala
+++ KataBankOCR/src/main/scala/org/shlrm/kataBankOCR/Numbers.scala
@@ -2,16 +2,45 @@ package org.shlrm.kataBankOCR
 
 object Numbers {
 
-  val  zero = " _ \n| |\n|_|"
-  val   one = "   \n  |\n  |"
-  val   two = " _ \n _|\n|_ "
-  val three = " _ \n _|\n _|"
-  val  four = "   \n|_|\n  |"
-  val  five = " _ \n|_ \n _|"
-  val   six = " _ \n|_ \n|_|"
-  val seven = " _ \n  |\n  |"
-  val eight = " _ \n|_|\n|_|"
-  val  nine = " _ \n|_|\n _|"
+  val  zero =
+    " _ \n" +
+    "| |\n" +
+    "|_|"
+  val   one =
+    "   \n" +
+    "  |\n" +
+    "  |"
+  val   two =
+    " _ \n" +
+    " _|\n" +
+    "|_ "
+  val three =
+    " _ \n" +
+    " _|\n" +
+    " _|"
+  val  four =
+    "   \n" +
+    "|_|\n  |"
+  val  five =
+    " _ \n" +
+    "|_ \n" +
+    " _|"
+  val   six =
+    " _ \n" +
+    "|_ \n" +
+    "|_|"
+  val seven =
+    " _ \n" +
+    "  |\n" +
+    "  |"
+  val eight =
+    " _ \n" +
+    "|_|\n" +
+    "|_|"
+  val  nine =
+    " _ \n" +
+    "|_|\n" +
+    " _|"
 
   def fromString(s: String): Char = s match {
     case `one`   => '1'
