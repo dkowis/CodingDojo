d85f37cd7f67ffdf7ddf6e4e8dfee94cef35139b
David Kowis <david.kowis@rackspace.com>
2013-03-11 17:37:05 -0500

    Stubbing tests. No implementation yet, due to compile failrues

---
+++ KataPotter/src/test/scala/CartSpec.scala
@@ -0,0 +1,44 @@
+import org.specs2.mutable._
+import io.Source
+
+class CartSpec extends Specification {
+
+  "Discounts are computed correctly" >> {
+    "for 0 books" in {
+      pending
+    }
+    "for 1 book" in {
+      pending
+    }
+    "for 2 different books" in {
+      pending
+    }
+    "for 2 identical books" in {
+      pending
+    }
+    "for 3 different books" in {
+      pending
+    }
+    "for 2 identical books and 1 other book" in {
+      pending
+    }
+    "for 4 different books" in {
+      pending
+    }
+    "for 5 different books" in {
+      pending
+    }
+
+    "for the example provided" in {
+      /*
+        2 copies of the first book
+        2 copies of the second book
+        2 copies of the third book
+        1 copy of the fourth book
+        1 copy of the fifth book
+        answer: 51.20
+      */
+      pending
+    }
+  }
+}
