c22442780f52426fc0c695a419571771108d2733
David Kowis <dkowis@shlrm.org>
2013-03-11 22:14:01 -0500

    Kind of working, got some neat functions making it go
    Doesn't quite work for all the test cases, but hey.

--- KataPotter/src/main/scala/Cart.scala
+++ KataPotter/src/main/scala/Cart.scala
@@ -1,8 +1,8 @@
 
 /**
-  * This is because I'm lazy and don't want to type much
-  */
-object Book{
+ * This is because I'm lazy and don't want to type much
+ */
+object Book {
   val first = "one"
   val second = "two"
   val third = "three"
@@ -17,6 +17,37 @@ case class Cart(books: Map[String, Int]) {
    * Just something to contain my discounts
    */
   object Discount {
+
+    /**
+     * Convert from a book count to a discount amount
+     * @param count
+     * @return
+     */
+    def apply(count: Int): Double = {
+      count match {
+        case 1 => one
+        case 2 => two
+        case 3 => three
+        case 4 => four
+        case 5 => five
+      }
+    }
+
+    /**
+     * Given a discount amount, tell me how many books that applies to
+     * @param discount
+     * @return
+     */
+    def count(discount: Double): Int = {
+      discount match {
+        case `one` => 1
+        case `two` => 2
+        case `three` => 3
+        case `four` => 4
+        case `five` => 5
+      }
+    }
+
     val one = 0
     val two = 0.05
     val three = 0.10
@@ -24,9 +55,24 @@ case class Cart(books: Map[String, Int]) {
     val five = 0.25
   }
 
-  def cost:Double = {
+  val discounts = {
+    def availableDiscounts(acc: List[Double], map: Map[String, Int]): List[Double] = {
+      //Find all available discounts, including permutations (two sets of four)
+      if (map.isEmpty) {
+        acc
+      } else {
+        val discount = Discount(map.keySet.size)
+        val newMap = map.mapValues(x => x - 1).filterNot(p => p._2 == 0)
+        availableDiscounts(discount :: acc, newMap)
+      }
+    }
 
-    def discount(count: Int, discount:Double):Double = {
+    availableDiscounts(List(), books)
+  }
+
+  def cost: Double = {
+
+    def discount(count: Int, discount: Double): Double = {
       println(s"doing discount for ${count} items with a discount of ${discount}")
       count * single * (1 - discount)
     }
@@ -34,12 +80,12 @@ case class Cart(books: Map[String, Int]) {
     if (books.isEmpty) {
       0
     } else {
-      if (books.keySet.size == 1){
-        println("doing something with things in here")
-        discount(books.get(books.keySet.head).get,Discount.one)
-      } else {
-        ???
-      }
+      println(s"Discounts are: ${discounts mkString "," }")
+      val leHax = discounts.foldLeft(0.0)((acc, v) => {
+        acc + discount(Discount.count(v), v)
+      })
+      println(s"So cost is: ${leHax}")
+      leHax
     }
   }
 
