747b74acf06ef284970804256dc76f49cc38c834
David Kowis <dkowis@shlrm.org>
2013-03-13 00:08:15 -0500

    I have no clue what's going on here
    I don't know why my map is not filtering properly. Nothing changes, and
    the map remains the same. This is unpossible. The only thing I can think
    of: It's not printlning corectly, but even then, *all* of the printlns
    show that the map never changes. This is impossible.

--- KataPotter/src/main/scala/Cart.scala
+++ KataPotter/src/main/scala/Cart.scala
@@ -55,27 +55,49 @@ case class Cart(books: Map[String, Int]) {
     val five = 0.25
   }
 
-  val discounts = {
-    def availableDiscounts(acc: List[Double], map: Map[String, Int]): List[Double] = {
-      if (map.isEmpty) {
+  val discounts: List[List[Double]] = {
+    def availableDiscounts(acc: List[Double], map: Map[String, Int], discountSize: Int): List[Double] = {
+      if (map.isEmpty || discountSize <= 1) {
         acc
       } else {
         val discount = Discount(map.keySet.size)
         val newMap = map.mapValues(x => x - 1).filterNot(p => p._2 == 0)
-        availableDiscounts(discount :: acc, newMap)
+        availableDiscounts(discount :: acc, newMap, discountSize)
       }
     }
 
-//    def allAvailableDiscounts(acc: List[Double], map: Map[String, Int], maxDiscount: Int):List[Double] = {
-//      if (map.isEmpty){
-//        acc
-//      } else {
-//        val discountSize = if (map.keySet.size > maxDiscount) maxDiscount else map.keySet.size
-//        //Create a new map, subtracting discountSize times from it...
-//      }
-//    }
+    def allAvailableDiscounts(acc: List[List[Double]], map: Map[String, Int], maxDiscount: Int): List[List[Double]] = {
+      //println(s"discounts for ${toString(map)}: ${acc mkString ","} max: ${maxDiscount}")
+      if (map.isEmpty) {
+        acc
+      } else {
+        if (maxDiscount == 1) {
+          acc
+        } else {
+          val discountSize = if (map.keySet.size > maxDiscount) maxDiscount else map.keySet.size
+          println(s"Discount size is ${discountSize}")
+          val toRemoveKeys = map.keySet.take(discountSize)
+          println(s"removing keys: ${toRemoveKeys mkString ","}")
 
-    availableDiscounts(List(), books)
+          val first = map.filter(p => {
+            val value = !toRemoveKeys.contains(p._1.toString)
+            println(s"removing ${p._1.toString} if it's in ${toRemoveKeys mkString ","}  value is ${value}")
+            value
+          })
+          println(s"first: ${toString(first)}")
+
+          val second = first.mapValues(v => v -1)
+          println(s"second: ${toString(second)}")
+
+          val newMap = (map.filterKeys(k => toRemoveKeys.contains(k)).mapValues(v => v - 1) ++ map.filterKeys(k => !toRemoveKeys.contains(k))).filterNot(p => p._2 == 0)
+          println(s"new map is: ${toString(newMap)}")
+          allAvailableDiscounts(availableDiscounts(List(), newMap, maxDiscount) :: acc, newMap, maxDiscount) ++
+            allAvailableDiscounts(availableDiscounts(List(), map, maxDiscount - 1) :: acc, map, maxDiscount - 1)
+        }
+      }
+    }
+
+    allAvailableDiscounts(List(), books, 5)
   }
 
   def cost: Double = {
@@ -88,11 +110,42 @@ case class Cart(books: Map[String, Int]) {
       0
     } else {
       val leHax = discounts.foldLeft(0.0)((acc, v) => {
-        acc + discount(Discount.count(v), v)
+        val one = v.foldLeft(-1.0)((a, v1) => {
+          a + discount(Discount.count(v1), v1)
+        })
+        //println(s"Comparing ${acc} with ${one} -- Discounts: ${v mkString ","}")
+        if (acc == -1.0) {
+          one
+        } else {
+          if (acc < one || one == 0) {
+            acc
+          } else {
+            one
+          }
+        }
       })
-      println(s"Discounts: ${discounts mkString ","} -- cost is: ${leHax}")
+      println(s"final cost is: ${leHax}")
       leHax
     }
   }
 
+  def toString(map: Map[String, Int]) = {
+    //GO from the mapped values to a set of strings
+    books.foldLeft(List[Int]())((acc:List[Int], p) => {
+      (1 to p._2).foldLeft(List[Int]())( (a:List[Int],i) => {
+         val wat = p._1 match {
+          case Book.first => 1
+          case Book.second => 2
+          case Book.third => 3
+          case Book.fourth => 4
+          case Book.fifth => 5
+        }
+        wat :: a
+      }) ++ acc
+    }) mkString ","
+  }
+
+  override def toString = {
+    toString(books)
+  }
 }
