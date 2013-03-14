4b6ab3d3cdd71bf510bb0fe60a8e78c31afca5eb
David Kowis <dkowis@shlrm.org>
2013-03-13 21:51:47 -0500

    Different strategerie

--- KataPotter/src/main/scala/org/shlrm/codingDojo/kataPotter/Cart2.scala
+++ KataPotter/src/main/scala/org/shlrm/codingDojo/kataPotter/Cart2.scala
@@ -4,7 +4,29 @@ case class Cart2(books: List[Int]) {
 
   val single = 8
 
-  val cost:Double = {
-    ???
+  val cost: Double = {
+    /**
+     * Produce a something that contains as many groups of unique books as possible
+     * Allows the specification of the size, to request a pair, or 3 or 4 or 5
+     * Should return all those groups, and then the leftovers. Maybe a list of discounts
+     * I can go from discount to book count as well.
+     * @param books
+     * @param size
+     * @return
+     */
+    def produceGroups(books:List[Int], size: Int) = {
+      ???
+    }
+    if (books.isEmpty) {
+      0.0
+    } else {
+      //TODO: find unique groups in the list of a specified size
+      if (books.size == 1) {
+        single
+      } else {
+        ???
+
+      }
+    }
   }
 }
