b1f32aba71b41e7232af9cfd982823a7c7172d73
David Kowis <dkowis@shlrm.org>
2013-03-14 22:58:32 -0500

    Got it working! I think

--- KataPotter/src/main/scala/org/shlrm/codingDojo/kataPotter/Cart.scala
+++ KataPotter/src/main/scala/org/shlrm/codingDojo/kataPotter/Cart.scala
@@ -28,7 +28,7 @@ case class Cart(books: Map[Int, Int]) {
 
     def discountsOfSize(acc: List[Double], myBooks: Map[Int, Int], size: Int): List[Double] = {
       //Need to look for a set of unique the same size
-      println(s"I'm looking for ${size} unique items in ${mappedString(myBooks)}")
+      //println(s"I'm looking for ${size} unique items in ${mappedString(myBooks)}")
 
       if (myBooks.keySet.isEmpty || size == 0) {
         acc
@@ -36,17 +36,22 @@ case class Cart(books: Map[Int, Int]) {
         if (hasUnique(myBooks, size)) {
           //All of the keys are in here, I can make a group of the requested size
           //TODO: need to only map however many are in the unique group size, not *all* the keys
-          val next = myBooks.mapValues(v => {
+          val toChange = myBooks.keySet.take(size)
+          val next = myBooks.map(p => {
             //Filter the map, removing one from the count of each item in the wants
-            v - 1
+            if (toChange.contains(p._1)) {
+              (p._1, p._2 - 1)
+            } else {
+              (p._1, p._2)
+            }
           }).filterNot(p => p._2 == 0)
           //make a discount of size
           val discount = Discount(size);
-          println(s"Appending a discount of ${discount}")
+          //println(s"Appending a discount of ${discount}")
           //again with the same size!
           discountsOfSize(discount :: acc, next, size)
         } else {
-          println(s"${size} unique items is more than ${mappedString(myBooks)} has")
+          //println(s"${size} unique items is more than ${mappedString(myBooks)} has")
           //Not all the things are in here, need to recurse with a smaller size
           discountsOfSize(acc, myBooks, size - 1)
         }
@@ -54,6 +59,7 @@ case class Cart(books: Map[Int, Int]) {
     }
 
     new Range(5, 1, -1).foldLeft(List[List[Double]]())((a, x) => {
+      //println(s"Finding discounts with max size of ${x}")
       discountsOfSize(List(), books, x) :: a
     })
   }
@@ -68,13 +74,14 @@ case class Cart(books: Map[Int, Int]) {
       0
     } else {
 
+      println("========= DISCOUNTS =============")
       println(discounts)
       println("=================================")
-      val leHax = discounts.foldLeft(0.0)((acc, v) => {
-        val one = v.foldLeft(-1.0)((a, v1) => {
+      val leHax = discounts.foldLeft(-1.0)((acc, v) => {
+        val one = v.foldLeft(0.0)((a, v1) => {
           a + discount(Discount.count(v1), v1)
         })
-        //println(s"Comparing ${acc} with ${one} -- Discounts: ${v mkString ","}")
+        println(s"Comparing ${acc} with ${one} -- Discounts: ${v mkString ","}")
         if (acc == -1.0) {
           one
         } else {
